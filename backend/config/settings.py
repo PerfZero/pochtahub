abf0fcaac66e8c88b0a2ffb48b5de7b7c3f0757a
PerfZero <gundenis1@gmail.com>
2025-12-06 18:37:01 +0300

    Add CSRF_TRUSTED_ORIGINS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,6 +132,12 @@ CORS_ALLOWED_ORIGINS = config(
     cast=lambda v: [s.strip() for s in v.split(',')]
 )
 
+CSRF_TRUSTED_ORIGINS = config(
+    'CSRF_TRUSTED_ORIGINS',
+    default='http://localhost:3000,http://127.0.0.1:3000,http://localhost:5173,http://127.0.0.1:5173',
+    cast=lambda v: [s.strip() for s in v.split(',')]
+)
+
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
         'Bearer': {
